1124c49b33b9b58fe62d950e01180626d10c4577
Abhranil Naha <abhranilnaha@gmail.com>
2015-05-03 19:36:37 -0700

    Added files for angularjs.

--- JobSearch/src/main/java/edu/sjsu/CourseRepository.java
+++ JobSearch/src/main/java/edu/sjsu/CourseRepository.java
@@ -2,7 +2,7 @@ package edu.sjsu;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface courseRepo extends MongoRepository<Course,String> {
+public interface CourseRepository extends MongoRepository<Course,String> {
 
     //public void save(int id, String shortName ,String name);
 
--- JobSearch/src/main/java/edu/sjsu/LinkedInController.java
+++ JobSearch/src/main/java/edu/sjsu/LinkedInController.java
@@ -1,135 +1,141 @@
 package edu.sjsu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.linkedin.api.*;
+import org.springframework.social.linkedin.api.Company;
+import org.springframework.social.linkedin.api.Job;
+import org.springframework.social.linkedin.api.JobSearchParameters;
+import org.springframework.social.linkedin.api.Jobs;
+import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.linkedin.api.LinkedInProfile;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.ArrayList;
 
 @Repository
 @Controller
 public class LinkedInController {
 
-    @Autowired
-    courseRepo cr;
+	@Autowired
+	CourseRepository cr;
 
-    private LinkedIn linkedIn;
+	private LinkedIn linkedIn;
 
-    private ConnectionRepository connectionRepository;
-    RestTemplate restTemplate = new RestTemplate();
+	private ConnectionRepository connectionRepository;
+	RestTemplate restTemplate = new RestTemplate();
 
+	@Inject
+	public LinkedInController(LinkedIn linkedIn,
+			ConnectionRepository connectionRepository) {
+		this.linkedIn = linkedIn;
+		this.connectionRepository = connectionRepository;
+	}
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String helloLinkedIn(Model model) {
+		if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+			return "redirect:/connect/linkedin";
+		}
+		model.addAttribute(linkedIn.profileOperations().getUserProfile());
+		List<LinkedInProfile> connections = linkedIn.connectionOperations()
+				.getConnections();
+		model.addAttribute("connections", connections);
 
-    @Inject
-    public LinkedInController(LinkedIn linkedIn, ConnectionRepository connectionRepository) {
-        this.linkedIn = linkedIn;
-        this.connectionRepository = connectionRepository;
-    }
+		return "hello";
+	}
 
-    @RequestMapping(value = "/",method=RequestMethod.GET)
-    public String helloLinkedIn(Model model) {
-        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
-            return "redirect:/connect/linkedin";
-        }
-        model.addAttribute(linkedIn.profileOperations().getUserProfile());
-        List<LinkedInProfile> connections = linkedIn.connectionOperations().getConnections();
-        model.addAttribute("connections", connections);
+	@RequestMapping(value = "/courses", method = RequestMethod.GET)
+	public String courses(Model model) throws RestClientException {
+		if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+			return "redirect:/connect/linkedin";
+		}
 
-        return "hello";
-    }
+		RestTemplate restTemplate = new RestTemplate();
+		Courses courses = restTemplate.getForObject(
+				"https://api.coursera.org/api/catalog.v1/courses",
+				Courses.class);
+		// System.out.println("Elements are" + courses.getElements() +
+		// "\n\n\n\n\n");
 
+		ArrayList<Course> elements = courses.getElements();
 
-    @RequestMapping(value = "/courses",method=RequestMethod.GET)
-    public String courses(Model model) throws RestClientException {
-        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
-            return "redirect:/connect/linkedin";
-        }
+		cr.save(elements);
+		model.addAttribute("courses", elements);
+		return "courses";
+	}
 
-        RestTemplate restTemplate = new RestTemplate();
-        Courses courses = restTemplate.getForObject("https://api.coursera.org/api/catalog.v1/courses", Courses.class);
-        //System.out.println("Elements are" + courses.getElements() + "\n\n\n\n\n");
+	@RequestMapping(value = "/skillMatch", method = RequestMethod.GET)
+	public String coursesMatch(Model model) {
+		if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+			return "redirect:/connect/linkedin";
+		}
 
-        ArrayList<Course> elements = courses.getElements();
+		List<Course> elements = cr.findAll();
+		List<String> skills = linkedIn.profileOperations().getUserProfileFull()
+				.getSkills();
 
-        cr.save(elements);
-        model.addAttribute("courses", elements);
-        return "courses";
-    }
+		// /cloudcomputing- Cloud Computing Concepts
+		for (int i = 0; i < elements.size(); i++) {
 
-    @RequestMapping(value = "/skillMatch",method=RequestMethod.GET)
-    public String coursesMatch(Model model) {
-        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
-            return "redirect:/connect/linkedin";
-        }
+			for (int j = 0; j < skills.size(); j++) {
+				if (elements.get(i).getShortName()
+						.equalsIgnoreCase(skills.get(j))) {
 
-        List<Course> elements = cr.findAll();
-        List<String> skills = linkedIn.profileOperations().getUserProfileFull().getSkills();
+					model.addAttribute("skill", skills.get(j));
+					model.addAttribute("course", elements.get(i).getShortName());
+					model.addAttribute("name", elements.get(i).getName());
 
-        ///cloudcomputing- Cloud Computing Concepts
-        for(int i=0 ;i<elements.size();i++)
-        {
+				}
+			}
 
-            for(int j=0 ;j<skills.size();j++) {
-                if (elements.get(i).getShortName().equalsIgnoreCase(skills.get(j))) {
+		}
 
-                    model.addAttribute("skill", skills.get(j));
-                    model.addAttribute("course", elements.get(i).getShortName());
-                    model.addAttribute("name",elements.get(i).getName());
+		/*
+		 * RestTemplate restTemplate = new RestTemplate(); Courses courses =
+		 * restTemplate
+		 * .getForObject("https://api.coursera.org/api/catalog.v1/courses",
+		 * Courses.class); ArrayList<Course> elements = courses.getElements();
+		 * 
+		 * cr.save(elements); model.addAttribute("courses", elements);
+		 */
 
-                }
-            }
+		return "skillMatch";
+	}
 
-        }
+	@RequestMapping(value = "/jobs", method = RequestMethod.GET)
+	public String joblistLinkedIn(Model model) {
 
-        /*RestTemplate restTemplate = new RestTemplate();
-        Courses courses = restTemplate.getForObject("https://api.coursera.org/api/catalog.v1/courses", Courses.class);
-        ArrayList<Course> elements = courses.getElements();
+		if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+			return "redirect:/connect/linkedin";
+		}
+		model.addAttribute(linkedIn.profileOperations().getUserProfile());
+		List<Company> companyList = linkedIn.companyOperations().getFollowing();
+		model.addAttribute("company", companyList);
+		return "jobs";
+	}
 
-        cr.save(elements);
-        model.addAttribute("courses", elements);*/
+	@RequestMapping(value = "/jobsList", method = RequestMethod.GET)
+	public String jobsPostedLinkedIn(Model model) {
 
-        return "skillMatch";
-    }
+		JobSearchParameters location = new JobSearchParameters();
+		if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+			return "redirect:/connect/linkedin";
+		}
+		Jobs jobs = linkedIn.jobOperations().searchJobs(location);
+		List<Job> jobList = jobs.getJobs();
+		model.addAttribute("JobSearch", jobList);
+		model.addAttribute(linkedIn.profileOperations().getUserProfile());
 
-    @RequestMapping(value = "/jobs",method=RequestMethod.GET)
-    public String joblistLinkedIn(Model model) {
-
-        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
-            return "redirect:/connect/linkedin";
-        }
-        model.addAttribute(linkedIn.profileOperations().getUserProfile());
-        List<Company> companyList = linkedIn.companyOperations().getFollowing();
-        model.addAttribute("company",companyList);
-        return "jobs";
-    }
-
-    @RequestMapping(value = "/jobsList",method=RequestMethod.GET)
-    public String jobsPostedLinkedIn(Model model) {
-
-       JobSearchParameters location = new JobSearchParameters();
-        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
-            return "redirect:/connect/linkedin";
-        }
-        Jobs jobs= linkedIn.jobOperations().searchJobs(location);
-        List<Job> jobList= jobs.getJobs();
-        model.addAttribute("JobSearch",jobList);
-        model.addAttribute(linkedIn.profileOperations().getUserProfile());
-
-        return "jobsList";
-    }
-
+		return "jobsList";
+	}
 
 }
